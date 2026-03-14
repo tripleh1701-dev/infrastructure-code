cceb27210b191db19171959f25681a8488b241e2
tripleh1701-dev <tripleh1701@gmail.com>
2026-03-14 07:49:26 +0530

    Update main.tf

--- infra/modules/github-oidc/main.tf
+++ infra/modules/github-oidc/main.tf
@@ -292,20 +292,3 @@ resource "aws_iam_role_policy" "cross_account" {
   })
 }
 
-# -----------------------------------------------------------------------------
-# Outputs
-# -----------------------------------------------------------------------------
-output "role_arn" {
-  description = "ARN of the GitHub Actions IAM role"
-  value       = aws_iam_role.github_actions.arn
-}
-
-output "role_name" {
-  description = "Name of the GitHub Actions IAM role"
-  value       = aws_iam_role.github_actions.name
-}
-
-output "oidc_provider_arn" {
-  description = "ARN of the GitHub OIDC provider"
-  value       = local.oidc_provider_arn
-}
